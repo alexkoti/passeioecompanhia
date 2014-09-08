7458c8abc7193009c32f7fa291aa2faef58a6298
Alex Koti <alexkoti@gmail.com>
2014-09-08 00:55:00 -0300

    tiny mce reduzido

--- wp-content/plugins/passeioecompanhia/inc/tinymce.php
+++ wp-content/plugins/passeioecompanhia/inc/tinymce.php
@@ -7,12 +7,6 @@
  * 
  */
 
-/* ========================================================================== */
-/* ADD ACTIONS/FILTERS ====================================================== */
-/* ========================================================================== */
-//FIXOS
-add_filter( 'mce_css', 'edit_mce_css' );
-add_filter( 'tiny_mce_before_init', 'set_the_editor_class' );
 
 
 /* ========================================================================== */
@@ -22,10 +16,13 @@ add_filter( 'tiny_mce_before_init', 'set_the_editor_class' );
  * Adiciona estilos css ao editor de texto do admin
  * O css adicionado é baseado no WYMeditor, com auxílos visuais presentes apenas na tela de postagem.
  */
+add_filter( 'mce_css', 'edit_mce_css' );
 function edit_mce_css($mce_css){
 	$mce_css = get_bloginfo('template_url') . '/css/site.css, ' . $mce_css;
 	return $mce_css;
 }
+
+add_filter( 'tiny_mce_before_init', 'set_the_editor_class' );
 function set_the_editor_class( $init ){
 	global $post;
 	if( isset($post->post_type) )
@@ -36,13 +33,22 @@ function set_the_editor_class( $init ){
 }
 
 /**
- * ==================================================
- * INSERIR SCRIPTS APÓS OS PLUGINS DE TINYMCE =======
- * ==================================================
- * 
+ * Configurar editor principal
  */
-// add_filter("mce_external_plugins", "add_myplugin_tinymce_plugin");
-function add_myplugin_tinymce_plugin($plugin_array) {
-   $plugin_array['dummy'] = FUNCTIONS . '/js/dummy.js");';
-   return $plugin_array;
+add_filter( 'mce_buttons', 'custom_editor_buttons' );
+function custom_editor_buttons( $buttons ){
+	/**/
+	$buttons = array(
+		'bold', 
+		'italic', 
+		'link', 
+		'unlink', 
+		'separator', 
+		'cleanup', 
+		'undo', 
+		'redo', 
+	);
+	/**/
+	//array_insert( $buttons, array( 'separator', 'code', 'charmap', 'separator' ), count($buttons) - 1 );
+	return $buttons;
 }
